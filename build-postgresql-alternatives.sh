0926a62c80fc1cf0fb8577c61655fdb515f3f562
KaiGai Kohei <kaigai@heterodb.com>
2019-09-05 08:50:45 +0000

    postgresql-alternatives-1.1 that includes PG-Strom commands

--- build-postgresql-alternatives.sh
+++ build-postgresql-alternatives.sh
@@ -7,9 +7,12 @@ cd `dirname $0`
 #--------------------------------
 mkdir -p ${SRCDIR}
 rm -rf ${RPMDIR}/*
-VERSION=`rpmspec --qf %{version} -q files/postgresql-alternatives.spec`
-RELEASE=`rpmspec --qf %{release} -q files/postgresql-alternatives.spec`
-RPMFILE="`rpmspec --rpms -q files/postgresql-alternatives.spec`.rpm"
+__SPECFILE=files/postgresql-alternatives.spec
+VERSION=`rpmspec --qf %{version} -q ${__SPECFILE}`
+RELEASE=`rpmspec --qf %{release} -q ${__SPECFILE}`
+RPMFILE=`rpmspec --rpms -q ${__SPECFILE}`.rpm
+SRPMFILE=`rpmspec --srpm -q ${__SPECFILE} | sed "s/\.noarch\$/\.src.rpm/g"`
+
 ARCH_LIST=x86_64
 
 SPECFILE=postgresql-alternatives.spec
@@ -20,12 +23,15 @@ test -e "$RPMDIR/noarch/${RPMFILE}" || (echo "RPM files missing"; exit 1)
 if [ -x ~/rpmsign.sh ];
 then
   ~/rpmsign.sh "$RPMDIR/noarch/${RPMFILE}" || (echo "failed on rpmsign.sh"; exit 1)
+  ~/rpmsign.sh "$SRPMDIR/${SRPMFILE}" || (echo "failed on rpmsign.sh"; exit 1)
 fi
 if [ "$INSTALL" -ne 0 ]; then
   for ARCH in ${ARCH_LIST}
   do
-    cp -f "$RPMDIR/noarch/${RPMFILE}" "docs/yum/${DISTRO}-${ARCH}/" || exit 1
+    cp -f "$RPMDIR/noarch/${RPMFILE}" "docs/yum/${DISTRO}-${ARCH}" || exit 1
     git add "docs/yum/${DISTRO}-${ARCH}/${RPMFILE}" || exit 1
+    cp -f "$SRPMDIR/${SRPMFILE}" "docs/yum/${DISTRO}-source" || exit 1
+    git add "docs/yum/${DISTRO}-source/${SRPMFILE}" || exit 1
   done
 else
   echo "NOTICE: installation onto docs/ was skipped"
--- files/postgresql-alternatives.spec
+++ files/postgresql-alternatives.spec
@@ -1,5 +1,5 @@
 Name: postgresql-alternatives
-Version: 1.0
+Version: 1.1
 Release: 1%{?dist}
 Summary: alternatives configuration for PostgreSQL
 Group: Applications/Databases
