c6fbbe4208814ff3e390041cd1f8f96cde303742
KaiGai Kohei <kaigai@heterodb.com>
2021-06-29 16:23:17 +0900

    fix build script

--- build-pg_strom.sh
+++ build-pg_strom.sh
@@ -25,50 +25,98 @@ rm -rf ${RPMDIR}/*
 set -- `echo "$VERSION" | tr '-' ' '`
 STROM_VERSION=$1
 STROM_RELEASE=$2
+NO_DEBUG_SOURCE="--undefine=_debugsource_packages"
+
+# xxx2arrow
+SPECFILES="${SPECDIR}/pg2arrow.spec ${SPECDIR}/mysql2arrow.spec ${SPECDIR}/pcap2arrow.spec"
+
+make -C "${GITDIR}" \
+     PGSTROM_VERSION=${STROM_VERSION} \
+     PGSTROM_RELEASE=${STROM_RELEASE} \
+     PGSTROM_GITHASH=${GITHASH} rpm-arrow
+RPMFILES=`rpmspec -q --rpms ${NO_DEBUG_SOURCE} ${SPECFILES}`
+SRPMFILE=`rpmspec -q --srpm ${NO_DEBUG_SOURCE} ${SPECFILES} | sed "s/${ARCH}\$/src/g"`
+
+for f in $RPMFILES
+do
+    FILE="$RPMDIR/${ARCH}/${f}.rpm"
+    test -e "$FILE" || abort "missing RPM file ($FILE)"
+    if [ -x ~/rpmsign.sh ]; then
+      ~/rpmsign.sh "$FILE" || abort "failed on rpmsign.sh ($FILE)"
+    fi
+    if [ "$INSTALL" -ne 0 ]; then
+        if echo "$f" | grep -q 'debuginfo'; then
+            DEST="docs/yum/${DISTRO}-debuginfo"
+	else
+            DEST="docs/yum/${DISTRO}-${ARCH}"
+	fi
+	cp -f "$FILE" "$DEST"  || abort "failed on copy"
+	git add "$DEST/$f.rpm" || abort "failed on git add"
+    fi
+done
+
+for f in $SRPMFILE
+do
+    FILE="$SRPMDIR/${f}.rpm"
+    test -e "$FILE" || abort "missing SRPM file ($FILE)"
+    if [ -x ~/rpmsign.sh ]; then
+      ~/rpmsign.sh "$FILE" || "failed on rpmsign.sh"
+    fi
+    if [ "$INSTALL" -ne 0 ]; then
+	DEST="docs/yum/${DISTRO}-source"
+	cp -f "$FILE" "$DEST"  || abort "failed on copy"
+	git add "$DEST/$f.rpm" || abort "failed on git add"
+    fi
+done
+
+# pg_strom module for each PG version
 for PVER in $PGSQL_VERSIONS
 do
-  SPECFILE="pg_strom-PG${PVER}.spec"
+  SPECFILE="${SPECDIR}/pg_strom-PG${PVER}.spec"
   make -C "${GITDIR}" \
       PG_CONFIG=/usr/pgsql-${PVER}/bin/pg_config \
       PGSTROM_VERSION=${STROM_VERSION} \
       PGSTROM_RELEASE=${STROM_RELEASE} \
       PGSTROM_GITHASH=${GITHASH} rpm
 
-  RPMFILES=`rpmspec -q --rpms --undefine=_debugsource_packages ${SPECDIR}/${SPECFILE}`
-  SRPMFILE=`rpmspec -q --srpm --undefine=_debugsource_packages ${SPECDIR}/${SPECFILE} | sed "s/${ARCH}\$/src.rpm/g"`
+  RPMFILES=`rpmspec -q --rpms ${NO_DEBUG_SOURCE} ${SPECFILE}`
+  SRPMFILE=`rpmspec -q --srpm ${NO_DEBUG_SOURCE} ${SPECFILE} | sed "s/${ARCH}\$/src/g"`
   for f in $RPMFILES;
   do
-    test -e "$RPMDIR/${ARCH}/${f}.rpm" || abort "missing RPM file"
-    if [ -x ~/rpmsign.sh ]; then
-      ~/rpmsign.sh "$RPMDIR/${ARCH}/${f}.rpm" || abort "failed on rpmsign.sh"
-    fi
-    if [ "$INSTALL" -ne 0 ]; then
-      if echo "$f" | grep -q 'debuginfo'; then
-        DEST="docs/yum/${DISTRO}-debuginfo"
-      else
-        DEST="docs/yum/${DISTRO}-${ARCH}"
+      FILE="$RPMDIR/${ARCH}/${f}.rpm"
+      test -e "$FILE" || abort "missing RPM file ($FILE)"
+      if [ -x ~/rpmsign.sh ]; then
+	  ~/rpmsign.sh "$FILE" || abort "failed on rpmsign.sh"
       fi
-      cp -f "$RPMDIR/${ARCH}/${f}.rpm" "$DEST" || abort "failed on copy"
-      git add "${DEST}/${f}.rpm" || abort "failed on git add"
-    fi
+      if [ "$INSTALL" -ne 0 ]; then
+	  if echo "$f" | grep -q 'debuginfo'; then
+              DEST="docs/yum/${DISTRO}-debuginfo"
+	  else
+              DEST="docs/yum/${DISTRO}-${ARCH}"
+	  fi
+	  cp -f "$FILE" "$DEST"  || abort "failed on copy"
+	  git add "$DEST/$f.rpm" || abort "failed on git add"
+      fi
   done
 
   for f in $SRPMFILE;
   do
-    test -e "$SRPMDIR/${f}" || abort "missing SRPM file"
-    if [ -x ~/rpmsign.sh ]; then
-      ~/rpmsign.sh "$SRPMDIR/${f}" || "failed on rpmsign.sh"
-    fi
-    if [ "$INSTALL" -ne 0 ]; then
-      cp -f "$SRPMDIR/${f}" "docs/yum/${DISTRO}-source" || abort "failed on copy"
-      git add "docs/yum/${DISTRO}-source/${f}" || abort "failed on git add"
-    fi
+      FILE="$SRPMDIR/${f}.rpm"
+      test -e "$FILE" || abort "missing SRPM file"
+      if [ -x ~/rpmsign.sh ]; then
+	  ~/rpmsign.sh "$FILE" || "failed on rpmsign.sh"
+      fi
+      if [ "$INSTALL" -ne 0 ]; then
+	  DEST="docs/yum/${DISTRO}-source"
+	  cp -f "$FILE" "$DEST"  || abort "failed on copy"
+	  git add "$DEST/$f.rpm" || abort "failed on git add"
+      fi
   done
 
   if [ "${STROM_RELEASE}" -le 1 ]; then
-    TARBALL="pg_strom-${STROM_VERSION}.tar.gz"
+      TARBALL="pg_strom-${STROM_VERSION}.tar.gz"
   else
-    TARBALL="pg_strom-${STROM_VERSION}-${STROM_RELEASE}.tar.gz"
+      TARBALL="pg_strom-${STROM_VERSION}-${STROM_RELEASE}.tar.gz"
   fi
   cp -f ${SRCDIR}/${TARBALL} docs/tgz || abort "failed on copy"
   git add docs/tgz/${TARBALL} || "failed on git add"
