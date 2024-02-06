598c9a316bd937a0d85bc537b84d849121cfebf4
KaiGai Kohei <kaigai@heterodb.com>
2024-02-06 15:16:19 +0000

    add heterodb-swdc-1.3-1.el9.noarch.rpm

--- build-postgresql-alternatives.sh
+++ build-postgresql-alternatives.sh
@@ -13,29 +13,19 @@ RELEASE=`rpmspec --qf %{release} -q ${__SPECFILE}`
 RPMFILE=`rpmspec --rpms -q ${__SPECFILE}`.rpm
 SRPMFILE=`rpmspec --srpm -q ${__SPECFILE} | sed "s/\.noarch\$/\.src.rpm/g"`
 
-ARCH_LIST=x86_64
-
 SPECFILE=postgresql-alternatives.spec
-cp -f files/${SPECFILE} ${SPECDIR}
+mkdir -p ${SPECDIR} && cp -f files/${SPECFILE} ${SPECDIR}
 
 rpmbuild -ba ${SPECDIR}/${SPECFILE} || (echo "filed on rpmbuild"; exit 1)
 test -e "$RPMDIR/noarch/${RPMFILE}" || (echo "RPM files missing"; exit 1)
-if [ -x ~/rpmsign.sh ];
-then
-  ~/rpmsign.sh "$RPMDIR/noarch/${RPMFILE}" || (echo "failed on rpmsign.sh"; exit 1)
-  ~/rpmsign.sh "$SRPMDIR/${SRPMFILE}" || (echo "failed on rpmsign.sh"; exit 1)
-fi
+rpm --addsign "$RPMDIR/noarch/${RPMFILE}" || (echo "failed on rpmsign"; exit 1)
+rpm --addsign "$SRPMDIR/${SRPMFILE}" || (echo "failed on rpmsign"; exit 1)
 
 if [ "$INSTALL" -ne 0 ]; then
   cp -f "$RPMDIR/noarch/${RPMFILE}" "docs/yum/${DISTRO}-noarch" || exit 1
-  git add "docs/yum/${DISTRO}-noarch/${RPMFILE}" || exit 1
-  for ARCH in ${ARCH_LIST}
-  do
-    ln -sf "../noarch/${RPMFILE}" "docs/yum/${DISTRO}-${ARCH}/${RPMFILE}" || exit 1
-    git add "docs/yum/${DISTRO}-${ARCH}/${RPMFILE}" || exit 1
-  done
   cp -f "$SRPMDIR/${SRPMFILE}" "docs/yum/${DISTRO}-source" || exit 1
-  git add "docs/yum/${DISTRO}-source/${SRPMFILE}" || exit 1
+  git add "docs/yum/${DISTRO}-noarch/${RPMFILE}" \
+          "docs/yum/${DISTRO}-source/${SRPMFILE}" || exit 1
 else
   echo "NOTICE: installation onto docs/ was skipped"
 fi
